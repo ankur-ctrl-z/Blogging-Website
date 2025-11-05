efe58adca13247629117bb57fc81c71ac91cdb4d
ANKUR SHARMA <ankur143513@gmail.com>
2025-11-05 11:55:07 +0530

    fix bug

--- common/dist/index.js
+++ common/dist/index.js
@@ -4,7 +4,7 @@ exports.updateBlogInput = exports.createBlogInput = exports.signinInput = export
 const zod_1 = require("zod");
 exports.signupInput = zod_1.z.object({
     username: zod_1.z.string().email(),
-    password: zod_1.z.string().min(6),
+    password: zod_1.z.string().min(5),
     name: zod_1.z.string().optional()
 });
 exports.signinInput = zod_1.z.object({
